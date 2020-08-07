b0e912a603c8679b7a76d0785aa3cd5d95d83aab
Chaynor Hsiao <chaynorhsiao@Chaynors-MacBook-Pro.local>
2020-08-06 20:55:19 -0700

    Starting to determine how to add values to workbooks

--- scratch.js
+++ scratch.js
@@ -1,1 +1,19 @@
-console.log('hi')
+XLSX = require('xlsx');
+/* output format determined by filename */
+const workbook = XLSX.utils.book_new();
+var ws_name = "SheetJS";
+
+/* make worksheet */
+var ws_data = [
+  [ "S", "h", "e", "e", "t", "J", "S" ],
+  [  1 ,  2 ,  3 ,  4 ,  5 ]
+];
+var ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+/* Add the worksheet to the workbook */
+XLSX.utils.book_append_sheet(workbook, ws, ws_name);
+console.log(ws['A1'])
+ws['F2'] = { v: 'S', t: 's' }
+console.log(ws)
+XLSX.writeFile(workbook, 'out.xlsb');
+/* at this point, out.xlsb is a file that you can distribute */
