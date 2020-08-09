5cc401dff66ce8bfefd30a53804a652550d41b5c
Chaynor Hsiao <chaynorhsiao@Chaynors-MacBook-Pro.local>
2020-08-08 20:05:43 -0700

    Added most of the numbers/formula, now attempting to change column width

--- scratch.js
+++ scratch.js
@@ -19,7 +19,6 @@ var ws_data = [
 	["Tel:", phone, '', '', '', "Date:", invoiceDate],
 	['', '', '', '', '', "Tax ID:", taxID],
 	[],
-	['DATES']
 ];
 var ws = XLSX.utils.aoa_to_sheet(ws_data);
 
@@ -27,14 +26,54 @@ var ws = XLSX.utils.aoa_to_sheet(ws_data);
 XLSX.utils.book_append_sheet(workbook, ws, ws_name);
 ws['!ref'] = 'A1:G41'
 
-let totalDaysWorked = 30
-offset = 6
-let date = new Date(2020, 8, 1)
-for (i = 1; i <= totalDaysWorked; i++) {
-	cell = `A${offset + i}`
-	date = new Date(date.getTime() + 86400000)
-	ws[cell] = { v: date, t: 'd'}
-}
+let totalDaysWorked = 22
+let dateInArrayForm = [2020, 8, 2]
+let hoursPerDay = 8
+let workDescription = 'Software Engineering'
+let hourlyRate = 40
 
-XLSX.writeFile(workbook, 'out.xlsb');
+offset = 8
+ws['A7'] = { t: 's', v: 'DATES'}
+ws['B7'] = { t: 's', v: 'HOURS'}
+ws['C7'] = { t: 's', v: 'WORK DESCRIPTION'}
+ws['D7'] = { t: 's', v: 'HOURLY RATE'}
+ws['E7'] = { t: 's', v: 'TOTAL'}
+
+ws['A4'] = { t: 's', v: 'Start Date'}
+ws['B4'] = { t: 'd', v: new Date([dateInArrayForm])}
+
+let dayNum = 0
+while (dayNum < totalDaysWorked) {
+	let currentCellNum = offset
+
+	let dateCell = `A${currentCellNum}`
+	ws[dateCell] = { t: 'd', f: `WORKDAY(B4, ${dayNum})`}
+
+	let hoursCell = `B${currentCellNum}`
+	ws[hoursCell] = { t: 'n', v: hoursPerDay}
+
+	let workDescCell = `C${currentCellNum}`
+	ws[workDescCell] = { t: 's', v: workDescription }
+
+	let hourlyRateCell = `D${currentCellNum}`
+	ws[hourlyRateCell] = { t: 's', v: `$${hourlyRate}`}
+
+	let totalCell = `E${currentCellNum}`
+	ws[totalCell] = { t: 'n', v: hoursPerDay * hourlyRate}
+
+	dayNum += 1
+	offset += 1
+}
+ws[`E${offset}`] = { t: 'n', f: `SUM(E8:E${offset - 1})`}
+ws[`D${offset}`] = { t: 's', v: 'Total:'}
+
+// let date = new Date(2020, 8, 1)
+
+// for (i = 1; i <= totalDaysWorked; i++) {
+// 	cell = `A${offset + i}`
+// 	date = new Date(date.getTime() + 86400000)
+// 	ws[cell] = { v: date, t: 'd'}
+// }
+
+XLSX.writeFile(workbook, 'out.xlsx');
 /* at this point, out.xlsb is a file that you can distribute */
